08c6fc0d2471d2eeab7f199b3ff640b677d01e62
Gemma Sagastegui <gemma.sagastegui01@estudiant.upf.edu>
2024-10-21 19:55:17 +0200

    Antiprime

--- src/antiprime.py
+++ src/antiprime.py
@@ -4,21 +4,42 @@
 def main() :
 	## YOU CODE SHOULD START HERE AST THE SAME
 	## IDENTATION AS THIS COMMENT
+        x = int(input())
+        
+        def cdiv(n):    
+                y = 0
+                i = 1
+                while (i <= n):
+                        if (n % i == 0):
+                                y += i
+                        i += 1
+                return y
+        def is_ap(n):
+                actual = cdiv(n)
+                i = 1
+                while i < n:
+                        if cdiv(i) >= actual:
+                                return False
+                        i += 1
+                return True
 
-
+        if is_ap(x):
+                print("antiprime")
+        else:
+                print("no antiprime")
+       
 	## THE LAST LINES OF YOUR CODE SHOULD EITHER
 	## RETURN THE VALUE "anti-prime" or "not anti-prime"
 	## REPLACE THE FOLLOWING LINE BY WHATEVER LINES
 	## OF CODE ALLOW THIS FUNCTION TO RETURN THE VALUE
 	## "anti-prime" or "not anti-prime"
-	return("anti-prime")
 
 ## DO NOT REMOVE THIS LINE BELOW
 if __name__ == "__main__" :
+	main()
 
 	## MODIFY THE LINE BELOW AND ADD BEFORE WHATEVER LINES ARE NECESSARY
 	## TO RUN THIS PROGRAM AS, FOR INSTANCE:
 	## $ python antiprime.py 6
 	## WHERE THE FIRST ARGUMENT IS A POSITIVE INTEGER NUMBER FOR WHICH
 	## YOU WANT TO FIGURE OUT WHETHER IS ANTI-PRIME OR NOT
-	print(main())
